89f1053a7d9d0671cf21db90c41c72f57972aea0
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-22 22:18:27 +0300

    Created two files *Created a .gitignore and README.md file

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -3,4 +3,9 @@ import static org.junit.Assert.*;
 
 public class HangmanTest {
 
+@Test
+public void doesCharMatch_isGuessCharEqualToAvailChar_true() {
+  Hangman hangman = new Hangman();
+  assertEquals(true, hangman.doesCharMatch("e"));
+}
 }
