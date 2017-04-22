6f2fdd513d697100e9f90d0f2da85f490c506589
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-22 22:57:10 +0300

    Added comments *Add comments public void section of my first test

--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -2,10 +2,16 @@ import org.junit.*;//import JUnit testing library
 import static org.junit.Assert.*;//import JUnit's assertion library
 
 public class HangmanTest {
-
+//annotation=> informs compiler that JUnit will be responsible for running this code
 @Test
+//all JUnit tests must be public void
+//doesCharMatch name methode this spec tests
+//isGuessCharEqualToAvailChar description behaviour spec tests
+//true output expecting for input we provide
 public void doesCharMatch_isGuessCharEqualToAvailChar_true() {
+
   Hangman hangman = new Hangman();
+
   assertEquals(true, hangman.doesCharMatch("e"));
 }
 }
