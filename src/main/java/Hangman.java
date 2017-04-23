2a752b4635fd5ea44cd3e994f20892a687e15168
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-23 08:22:12 +0300

    Change first test
    *In HangmanTest.java file change test to check if new instance is created correctly

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,8 +1,11 @@
-public class Hangman {
-public boolean doesCharMatch(String letter) {
-  return letter == "e";
+ public class Hangman {
+// public boolean doesCharMatch(String letter) {
+//   return letter == "e";
+// }
+// public boolean isCharGuessed(String letter) {
+//   return letter == "c";
+// }
+public Hangman(String answer, String guessedLetters) {
+  
 }
-public boolean isCharGuessed(String letter) {
-  return letter == "c";
-}
-}
+ }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -1,36 +1,46 @@
-import org.junit.*;//import JUnit testing library
-import static org.junit.Assert.*;//import JUnit's assertion library
+// import org.junit.*;//import JUnit testing library
+// import static org.junit.Assert.*;//import JUnit's assertion library
+//
+// public class HangmanTest {
+// //annotation=> informs compiler that JUnit will be responsible for running this code
+// @Test
+// //all JUnit tests must be public void
+// //doesCharMatch name methode this spec tests
+// //isGuessCharEqualToAvailChar description behaviour spec tests
+// //true output expecting for input we provide
+// public void doesCharMatch_isGuessCharEqualToAvailChar_true() {
+// //Hangman => class hangman => object
+//   Hangman hangman = new Hangman();
+// //assertEquals method instructs JUnit to compare two arguments
+// //Checking whether hangman.doesCharMatch("e") successfully returns true
+//   assertEquals(true, hangman.doesCharMatch("e"));
+// }
+//
+// @Test
+// public void doesCharMatch_isGuessCharEqualToAvailChar_false() {
+//   Hangman hangman = new Hangman();
+//   assertEquals(false, hangman.doesCharMatch("f"));
+// }
+//
+// @Test
+// public void isCharGuessed_isCharEqualGussedChar_true() {
+//   Hangman hangman = new Hangman();
+//   assertEquals(true, hangman.isCharGuessed("c"));
+// }
+//
+// @Test
+// public void isCharGuessed_isCharEqualGussedChar_false() {
+//   Hangman hangman = new Hangman();
+//   assertEquals(false, hangman.isCharGuessed("d"));
+// }
+// }
+
+import org.junit.*;
+import static org.junit.Assert.*;
 
 public class HangmanTest {
-//annotation=> informs compiler that JUnit will be responsible for running this code
-@Test
-//all JUnit tests must be public void
-//doesCharMatch name methode this spec tests
-//isGuessCharEqualToAvailChar description behaviour spec tests
-//true output expecting for input we provide
-public void doesCharMatch_isGuessCharEqualToAvailChar_true() {
-//Hangman => class hangman => object
-  Hangman hangman = new Hangman();
-//assertEquals method instructs JUnit to compare two arguments
-//Checking whether hangman.doesCharMatch("e") successfully returns true
-  assertEquals(true, hangman.doesCharMatch("e"));
-}
-
-@Test
-public void doesCharMatch_isGuessCharEqualToAvailChar_false() {
-  Hangman hangman = new Hangman();
-  assertEquals(false, hangman.doesCharMatch("f"));
-}
-
-@Test
-public void isCharGuessed_isCharEqualGussedChar_true() {
-  Hangman hangman = new Hangman();
-  assertEquals(true, hangman.isCharGuessed("c"));
-}
-
-@Test
-public void isCharGuessed_isCharEqualGussedChar_false() {
-  Hangman hangman = new Hangman();
-  assertEquals(false, hangman.isCharGuessed("d"));
+public void newHangman_createsInstance() {
+  Hangman hangman = new Hangman("dry", "abc");
+  assertEquals(true, hangman instanceof Hangman);
 }
 }
