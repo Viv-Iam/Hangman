d761c51ab3275dcad7b77a0a394c8cf616e3fd6e
Viv-Iam <vivian.opondoh@gmail.com>
2017-04-23 10:43:21 +0300

    Modified two files
    *Modified HangmanTest.java so that new instance can take in two parameters a string and an array
    *Modified my constructor to have an array and a string

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,20 +1,21 @@
  public class Hangman {
    private String mAnswer;
-   private String mGuessedLetters;
+   private String[] mGuessedLetters;
 // public boolean doesCharMatch(String letter) {
 //   return letter == "e";
 // }
 // public boolean isCharGuessed(String letter) {
 //   return letter == "c";
 // }
-public Hangman(String answer, String guessedLetters) {
+public Hangman(String answer, String[] guessedLetters) {
 mAnswer = answer;
+mGuessedLetters = guessedLetters;
 }
 public String getAnswer() {
   return mAnswer;
 }
-public String getGuessedLetters() {
-  return "abc";
+public String[] getGuessedLetters() {
+  return mGuessedLetters;
 }
 public boolean isAnswer(String input) {
   return mAnswer == input;
